38fb12f8e359f07eef36d32c724592b60ea16be6
Ryan <s160721046@student.ubaya.ac.id>
2025-01-04 17:05:44 +0700

    lupa tak save wkwkwk

--- database/seeders/FasumSeeder.php
+++ database/seeders/FasumSeeder.php
@@ -14,7 +14,7 @@ class FasumSeeder extends Seeder
     public function run()
     {
         DB::table('m_fasum')->insert([
-            ['idfasum' => 1, 'm_dinas_iddinas' => 1, 'nama' => 'Tiang Listrik', 'luas_fasum' => 4, 'kondisi_fasum' => 'Bengkok & penyok', 'asal_fasum' => 'Swasta', 'lat' => '-7.3150508016307905', 'lng' => '112.76669740676881', 'gambar' => '-', 'status_aktif' => 0],
+            ['idfasum' => 1, 'm_dinas_iddinas' => 1, 'nama' => 'Tiang Listrik', 'luas_fasum' => 4, 'kondisi_fasum' => 'Bengkok & penyok', 'asal_fasum' => 'Swasta', 'lat' => '0', 'lng' => '0', 'gambar' => '-', 'status_aktif' => 0],
             ['idfasum' => 2, 'm_dinas_iddinas' => 1, 'nama' => '', 'luas_fasum' => 421, 'kondisi_fasum' => 'tess', 'asal_fasum' => 'Swasta', 'lat' => '-7.3150508016307905', 'lng' => '112.76669740676881', 'gambar' => NULL, 'status_aktif' => 0],
             ['idfasum' => 3, 'm_dinas_iddinas' => 1, 'nama' => 'Halte A', 'luas_fasum' => 421, 'kondisi_fasum' => 'test', 'asal_fasum' => 'Swasta', 'lat' => '-7.3150508016307905', 'lng' => '112.76729822158815', 'gambar' => NULL, 'status_aktif' => 0],
             ['idfasum' => 4, 'm_dinas_iddinas' => 1, 'nama' => 'Halte B', 'luas_fasum' => 421, 'kondisi_fasum' => 'test', 'asal_fasum' => 'Swasta', 'lat' => '-7.3125459', 'lng' => '112.7740438', 'gambar' => NULL, 'status_aktif' => 0],
