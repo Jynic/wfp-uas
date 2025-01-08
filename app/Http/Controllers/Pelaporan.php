104975203f6dd06b9ae5d0b848ea01822ae231ad
Ryan <s160721046@student.ubaya.ac.id>
2025-01-09 02:50:01 +0700

    nabrak

--- app/Http/Controllers/Pelaporan.php
+++ app/Http/Controllers/Pelaporan.php
@@ -57,30 +57,6 @@ class Pelaporan extends Controller
         $data = $request->all();
         $id = $data['id'];
 
-<<<<<<< Updated upstream
-        $result = DB::table('t_pelaporan as p')
-            ->join('t_pelaporan_detail as pd', 'p.idpelaporan', '=', 'pd.t_pelaporan_idpelaporan')
-            ->join('m_user as u', 'p.iduser', '=', 'u.iduser')
-            ->join('m_fasum as f', 'pd.m_fasum_idfasum', '=', 'f.idfasum')
-            ->where('p.status_aktif', 1)
-            ->where('p.idpelaporan', $id)
-            ->select(
-                'p.idpelaporan AS id',
-                'p.nomor',
-                'p.tgl_pelaporan',
-                'p.status_pelaporan',
-                'p.keterangan',
-                'p.status_aktif',
-                'u.nama AS nama_user',
-                'u.iduser AS id_user',
-                'f.nama AS nama_fasum',
-                'f.idfasum AS id_fasum',
-                'pd.status_perbaikkan AS status_perbaikkan',
-                'pd.foto_fasum AS foto_fasum',
-                'pd.keterangan AS keterangan_fasum'
-            )
-            ->get();
-=======
         $result = DB::select("
         SELECT 
         p.idpelaporan AS id,
@@ -105,7 +81,6 @@ class Pelaporan extends Controller
             m_fasum f ON pd.m_fasum_idfasum = f.idfasum
         WHERE 
             p.status_aktif = 1 and p.idpelaporan = :id;", ['id' => $id]);
->>>>>>> Stashed changes
         return json_encode($result);
     }
 
@@ -202,44 +177,6 @@ class Pelaporan extends Controller
     public function getData(Request $request)
     {
         $id = $request->input('id');
-<<<<<<< Updated upstream
-        $data = DB::table('t_pelaporan as p')
-            ->leftJoin('t_pelaporan_detail as pd', 'p.idpelaporan', '=', 'pd.t_pelaporan_idpelaporan')
-            ->leftJoin('m_staff as s', 'p.idm_staff', '=', 's.idm_staff')
-            ->leftJoin('m_user as u', 'p.iduser', '=', 'u.iduser')
-            ->leftJoin('m_fasum as f', 'pd.m_fasum_idfasum', '=', 'f.idfasum')
-            ->where('p.status_aktif', 1)
-            ->groupBy(
-                'p.idpelaporan',
-                'p.nomor',
-                'p.tgl_pelaporan',
-                'p.status_pelaporan',
-                'p.keterangan',
-                'p.status_aktif',
-                's.nama',
-                's.idm_staff',
-                'u.nama',
-                'u.iduser'
-            )
-            ->select(
-                'p.idpelaporan AS id',
-                'p.nomor',
-                'p.tgl_pelaporan',
-                'p.status_pelaporan',
-                'p.keterangan',
-                'p.status_aktif',
-                's.nama AS nama_staff',
-                's.idm_staff AS id_staff',
-                'u.nama AS nama_user',
-                'u.iduser AS id_user',
-                DB::raw('GROUP_CONCAT(f.nama SEPARATOR ", ") AS nama_fasum'),
-                DB::raw('GROUP_CONCAT(f.idfasum SEPARATOR ", ") AS id_fasum'),
-                DB::raw('GROUP_CONCAT(pd.status_perbaikkan SEPARATOR ", ") AS status_perbaikkan'),
-                DB::raw('GROUP_CONCAT(pd.foto_fasum SEPARATOR ", ") AS foto_fasum'),
-                DB::raw('GROUP_CONCAT(pd.keterangan SEPARATOR ", ") AS keterangan_fasum')
-            )
-            ->get();
-=======
         $data = DB::select("SELECT 
     p.idm_staff,
     p.idpelaporan AS id,
@@ -281,7 +218,6 @@ class Pelaporan extends Controller
         u.nama, 
         u.iduser;
         ");
->>>>>>> Stashed changes
 
         $pelaporan = [];
         foreach ($data as $key => $value) {
@@ -328,26 +264,9 @@ class Pelaporan extends Controller
                 $value->nama_user,
                 $value->nama_fasum,
                 ($value->status_aktif == 1) ?
-<<<<<<< Updated upstream
-                '<span class="badge bg-success">Active</span>' :
-                '<span class="badge bg-danger">Inactive</span>',
-                '<div class="d-flex justify-content-center">
-                    <a href="javascript:void(0)" class="btn btn-primary btn-sm" onclick="detail(' . $value->id . ')">
-                        <i class="bx bx-info-circle"></i>
-                    </a>' .
-                '
-                    <a href="javascript:void(0)" class="btn btn-primary btn-sm ms-3" onclick="edit(' . $value->id . ')">
-                        <i class="bx bx-edit-alt"></i>
-                    </a>
-                    <a href="javascript:void(0)" class="btn btn-danger btn-sm ms-3" onclick="hapus(' . $value->id . ')">
-                        <i class="bx bx-trash"></i>
-                    </a>' .
-                '</div>'
-=======
                     '<span class="badge bg-success">Active</span>' :
                     '<span class="badge bg-danger">Inactive</span>',
                 $action_buttons
->>>>>>> Stashed changes
             );
         }
         // Kirim data dalam format JSON
@@ -380,48 +299,10 @@ class Pelaporan extends Controller
             );
 
         if ($dateFilter) {
-            $dateRange = Carbon::now()->subDays((int) $dateFilter);
+            $dateRange = Carbon::now()->subDays((int)$dateFilter);
             $query->where('p.tgl_pelaporan', '>=', $dateRange);
         }
 
-<<<<<<< Updated upstream
-        $data = DB::table('t_pelaporan as p')
-            ->leftJoin('t_pelaporan_detail as pd', 'p.idpelaporan', '=', 'pd.t_pelaporan_idpelaporan')
-            ->leftJoin('m_staff as s', 'p.idm_staff', '=', 's.idm_staff')
-            ->leftJoin('m_user as u', 'p.iduser', '=', 'u.iduser')
-            ->leftJoin('m_fasum as f', 'pd.m_fasum_idfasum', '=', 'f.idfasum')
-            ->where('p.status_aktif', 1)
-            ->groupBy(
-                'p.idpelaporan',
-                'p.nomor',
-                'p.tgl_pelaporan',
-                'p.status_pelaporan',
-                'p.keterangan',
-                'p.status_aktif',
-                's.nama',
-                's.idm_staff',
-                'u.nama',
-                'u.iduser'
-            )
-            ->select(
-                'p.idpelaporan as id',
-                'p.nomor',
-                'p.tgl_pelaporan',
-                'p.status_pelaporan',
-                'p.keterangan',
-                'p.status_aktif',
-                's.nama as nama_staff',
-                's.idm_staff as id_staff',
-                'u.nama as nama_user',
-                'u.iduser as id_user',
-                DB::raw('GROUP_CONCAT(f.nama SEPARATOR ", ") AS nama_fasum'),
-                DB::raw('GROUP_CONCAT(f.idfasum SEPARATOR ", ") AS id_fasum'),
-                DB::raw('GROUP_CONCAT(pd.status_perbaikkan SEPARATOR ", ") AS status_perbaikkan'),
-                DB::raw('GROUP_CONCAT(pd.foto_fasum SEPARATOR ", ") AS foto_fasum'),
-                DB::raw('GROUP_CONCAT(pd.keterangan SEPARATOR ", ") AS keterangan_fasum')
-            )
-            ->get();
-=======
         $data = $query->select(
             'p.idpelaporan as id',
             'p.nomor',
@@ -438,7 +319,6 @@ class Pelaporan extends Controller
             DB::raw('GROUP_CONCAT(pd.foto_fasum SEPARATOR ", ") AS foto_fasum'),
             DB::raw('GROUP_CONCAT(pd.keterangan SEPARATOR ", ") AS keterangan_fasum')
         )->get();
->>>>>>> Stashed changes
 
         $pelaporan = [];
         foreach ($data as $key => $value) {
@@ -450,8 +330,8 @@ class Pelaporan extends Controller
                 $value->nama_user,
                 $value->nama_fasum,
                 ($value->status_aktif == 1) ?
-                '<span class="badge bg-success">Active</span>' :
-                '<span class="badge bg-danger">Inactive</span>',
+                    '<span class="badge bg-success">Active</span>' :
+                    '<span class="badge bg-danger">Inactive</span>',
                 '<div class="d-flex justify-content-center">
                     <a href="javascript:void(0)" class="btn btn-primary btn-sm" onclick="detail(' . $value->id . ')">
                         <i class="bx bx-info-circle"></i>
@@ -466,11 +346,10 @@ class Pelaporan extends Controller
     public function getKota(Request $request)
     {
         $search_term = $request->input('search');
-        $data = DB::table('m_kota_kabupaten')
-            ->where('status_aktif', 1)
-            ->where('nama', 'like', '%' . $search_term . '%')
-            ->select('idkota_kabupaten as id', 'nama')
-            ->get();
+        $data = DB::select('SELECT kk.idkota_kabupaten AS id,
+        kk.nama
+        FROM m_kota_kabupaten kk
+        WHERE kk.status_aktif = 1 AND kk.nama LIKE :search', ['search' => '%' . $search_term . '%']);
         $kota = [];
         foreach ($data as $key => $row) {
             $kota[] = array(
@@ -487,11 +366,7 @@ class Pelaporan extends Controller
     public function getDataStaff(Request $request)
     {
         $search_term = $request->input('search');
-        $data = DB::table('m_staff')
-            ->where('status_aktif', 1)
-            ->where('nama', 'like', '%' . $search_term . '%')
-            ->select('idm_staff as id', 'nama')
-            ->get();
+        $data = DB::select('select s.idm_staff as id, s.nama from m_staff s where s.status_aktif = 1 and s.nama like :search', ['search' => '%' . $search_term . '%']);
         $dinas = [];
         foreach ($data as $key => $row) {
             $dinas[] = array(
@@ -508,11 +383,7 @@ class Pelaporan extends Controller
     public function getDataUser(Request $request)
     {
         $search_term = $request->input('search');
-        $data = DB::table('m_user')
-            ->where('status_aktif', 1)
-            ->where('nama', 'like', '%' . $search_term . '%')
-            ->select('iduser as id', 'nama')
-            ->get();
+        $data = DB::select('select u.iduser as id, u.nama from m_user u where u.status_aktif = 1 and u.nama like :search', ['search' => '%' . $search_term . '%']);
         $dinas = [];
         foreach ($data as $key => $row) {
             $dinas[] = array(
@@ -529,11 +400,7 @@ class Pelaporan extends Controller
     public function getDataFasum(Request $request)
     {
         $search_term = $request->input('search');
-        $data = DB::table('m_fasum')
-            ->where('status_aktif', 1)
-            ->where('nama', 'like', '%' . $search_term . '%')
-            ->select('idfasum as id', 'nama')
-            ->get();
+        $data = DB::select('select f.idfasum as id, f.nama from m_fasum f where f.status_aktif = 1 and f.nama like :search', ['search' => '%' . $search_term . '%']);
         $dinas = [];
         foreach ($data as $key => $row) {
             $dinas[] = array(
@@ -613,37 +480,13 @@ class Pelaporan extends Controller
 
     public function GetNomor()
     {
-        $nomor = Pelaporan_model::count('idpelaporan') + 1;
-        return intval($nomor);
+        $nomor = DB::select('SELECT count(idpelaporan) + 1 as nomor FROM t_pelaporan');
+        return intval($nomor[0]->nomor);
     }
     public function detail(Request $request)
     {
         $data = $request->all();
         $id = $data['id'];
-<<<<<<< Updated upstream
-        $data = DB::table('t_pelaporan as p')
-            ->leftJoin('t_pelaporan_detail as pd', 'p.idpelaporan', '=', 'pd.t_pelaporan_idpelaporan')
-            ->leftJoin('m_user as u', 'p.iduser', '=', 'u.iduser')
-            ->leftJoin('m_fasum as f', 'pd.m_fasum_idfasum', '=', 'f.idfasum')
-            ->where('p.status_aktif', 1)
-            ->where('p.idpelaporan', $id)
-            ->select(
-                'p.idpelaporan AS id',
-                'p.nomor',
-                'p.tgl_pelaporan',
-                'p.status_pelaporan',
-                'p.keterangan',
-                'p.status_aktif',
-                'u.nama AS nama_user',
-                'u.iduser AS id_user',
-                'f.nama AS nama_fasum',
-                'f.idfasum AS id_fasum',
-                'pd.status_perbaikkan AS status_perbaikkan',
-                'pd.foto_fasum AS foto_fasum',
-                'pd.keterangan AS keterangan_fasum'
-            )
-            ->get();
-=======
         $data = DB::select("
         SELECT 
         p.idpelaporan AS id,
@@ -668,7 +511,6 @@ class Pelaporan extends Controller
             m_fasum f ON pd.m_fasum_idfasum = f.idfasum
         WHERE 
             p.status_aktif = 1 and p.idpelaporan = :id;", ['id' => $id]);
->>>>>>> Stashed changes
 
         $pelaporan = [];
         foreach ($data as $key => $value) {
@@ -701,30 +543,6 @@ class Pelaporan extends Controller
     {
         $data = $request->all();
         $id = $data['id'];
-<<<<<<< Updated upstream
-        $data = DB::table('t_pelaporan as p')
-            ->join('t_pelaporan_detail as pd', 'p.idpelaporan', '=', 'pd.t_pelaporan_idpelaporan')
-            ->join('m_user as u', 'p.iduser', '=', 'u.iduser')
-            ->join('m_fasum as f', 'pd.m_fasum_idfasum', '=', 'f.idfasum')
-            ->where('p.status_aktif', 1)
-            ->where('p.idpelaporan', $id)
-            ->select(
-                'p.idpelaporan AS id',
-                'p.nomor',
-                'p.tgl_pelaporan',
-                'p.status_pelaporan',
-                'p.keterangan',
-                'p.status_aktif',
-                'u.nama AS nama_user',
-                'u.iduser AS id_user',
-                'f.nama AS nama_fasum',
-                'f.idfasum AS id_fasum',
-                'pd.status_perbaikkan AS status_perbaikkan',
-                'pd.foto_fasum AS foto_fasum',
-                'pd.keterangan AS keterangan_fasum'
-            )
-            ->get();
-=======
         $data = DB::select("
         SELECT 
         p.idpelaporan AS id,
@@ -749,7 +567,6 @@ class Pelaporan extends Controller
             m_fasum f ON pd.m_fasum_idfasum = f.idfasum
         WHERE 
             p.status_aktif = 1 and p.idpelaporan = :id;", ['id' => $id]);
->>>>>>> Stashed changes
 
         $pelaporan = [];
         foreach ($data as $key => $value) {
